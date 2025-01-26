cbe9ff2693e0d9aafd2964a91f3af7db088d0fb6
tomoyuki <>
2025-01-26 21:46:44 +0900

    fix: 静的ファイルの設定を簡略化

--- construction_management/settings.py
+++ construction_management/settings.py
@@ -84,8 +84,13 @@ TIME_ZONE = "Asia/Tokyo"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# 静的ファイルの設定
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# メディアファイルの設定
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
